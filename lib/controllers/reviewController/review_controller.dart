134b8465a4935c1932cd803882e0b19e90493497
EwigLife <96788793+syedzainqadri@users.noreply.github.com>
2023-03-25 15:24:33 +0500

    all controllers and bindings made

---
+++ lib/controllers/reviewController/review_controller.dart
@@ -0,0 +1,159 @@
+import 'dart:convert';
+
+import 'package:frontend_grounda/models/reviewModel/review_model.dart';
+import 'package:frontend_grounda/utils/global_variable.dart';
+import 'package:get/get.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:http/http.dart' as http;
+
+class ReviewsController extends GetxController {
+  var reviews = <ReviewsModel>[].obs;
+  final Box<dynamic> tokenHiveBox = Hive.box('token');
+  var token = ''.obs;
+  var isLoading = false.obs;
+
+  @override
+  void onInit() {
+    super.onInit();
+    token.value = tokenHiveBox.get('token');
+  }
+
+  Future<void> getAll() async {
+    isLoading.value = true;
+    var response = await http.get(
+      Uri.parse(
+        baseUrl + allReview,
+      ),
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": "Bearer $token"
+      },
+    );
+    if (response.statusCode == 200 && response.body != 'null') {
+      reviews.value = reviewsModelFromJson(response.body);
+      isLoading.value = false;
+    } else {
+      Get.snackbar('Error', response.body,
+          snackPosition: SnackPosition.BOTTOM, maxWidth: 400);
+      isLoading.value = false;
+    }
+  }
+
+  Future<void> getbyPostId(String id) async {
+    isLoading.value = true;
+    var response = await http.get(
+      Uri.parse(
+        baseUrl + reviewByPostId + id,
+      ),
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": "Bearer $token"
+      },
+    );
+    if (response.statusCode == 200 && response.body != 'null') {
+      reviews.value = reviewsModelFromJson(response.body);
+      isLoading.value = false;
+    } else {
+      Get.snackbar('Error', response.body,
+          snackPosition: SnackPosition.BOTTOM, maxWidth: 400);
+      isLoading.value = false;
+    }
+  }
+
+  Future<void> create(
+    int userId,
+    double rating,
+    String message,
+    int postId,
+  ) async {
+    isLoading.value = true;
+    var bodyPrepare = {
+      "userId": 1,
+      "rating": 5.0,
+      "message": "message",
+      "postId": 4
+    };
+
+    var response = await http.post(
+      Uri.parse(
+        baseUrl + createReview,
+      ),
+      body: jsonEncode(bodyPrepare),
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": "Bearer $token"
+      },
+    );
+    if (response.statusCode == 200 && response.body != 'null') {
+      reviews.addAll(reviewsModelFromJson(response.body));
+      isLoading.value = false;
+    } else {
+      Get.snackbar('Error', response.body,
+          snackPosition: SnackPosition.BOTTOM, maxWidth: 400);
+      isLoading.value = false;
+    }
+  }
+
+  Future<void> updatePaymentMethod(
+    int id,
+    int userId,
+    double rating,
+    String message,
+    int postId,
+  ) async {
+    isLoading.value = true;
+    var bodyPrepare = {
+      "id": id,
+      "userId": 1,
+      "rating": 5.0,
+      "message": "message",
+      "postId": 4
+    };
+
+    var response = await http.put(
+      Uri.parse(
+        baseUrl + updateReviewUrl,
+      ),
+      body: jsonEncode(bodyPrepare),
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": "Bearer $token"
+      },
+    );
+    if (response.statusCode == 200 && response.body != 'null') {
+      getAll();
+      isLoading.value = false;
+    } else {
+      Get.snackbar('Error', response.body,
+          snackPosition: SnackPosition.BOTTOM, maxWidth: 400);
+      isLoading.value = false;
+    }
+  }
+
+  Future<void> delete(
+    String id,
+  ) async {
+    isLoading.value = true;
+    var response = await http.delete(
+      Uri.parse(
+        baseUrl + deleteReview + id,
+      ),
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": "Bearer $token"
+      },
+    );
+    if (response.statusCode == 200 && response.body != 'null') {
+      getAll();
+      var deletedReview = jsonDecode(response.body);
+      var review = deletedReview['message'];
+      Get.snackbar('Product Deleted', 'The Product: $review has been deleted',
+          snackPosition: SnackPosition.BOTTOM, maxWidth: 400);
+      isLoading.value = false;
+    } else {
+      Get.snackbar('Error', response.body,
+          snackPosition: SnackPosition.BOTTOM, maxWidth: 400);
+      isLoading.value = false;
+    }
+  }
+}
